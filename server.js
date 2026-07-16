656e2ec95d028fc27aa9a2b44ea470e2e2db733c
TalhaJubair35 <jubairtalha15@gmail.com>
2026-07-17 03:01:28 +0600

    bug didnt solve

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ import userRouter from "./routes/userRoutes.js";
 import ownerRouter from "./routes/ownerRoutes.js";
 import bookingRouter from "./routes/bookingRoutes.js";
 
-const app = express();
+// Initialize Express App
+const app = express()
 
-await connectDB();
+// Connect Database
+await connectDB()
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Server is running");
-});
+app.get('/', (req, res)=> res.send("Server is running"))
+app.use('/api/user', userRouter)
+app.use('/api/owner', ownerRouter)
+app.use('/api/bookings', bookingRouter)
 
-app.use("/api/user", userRouter);
-app.use("/api/owner", ownerRouter);
-app.use("/api/bookings", bookingRouter);
-
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
